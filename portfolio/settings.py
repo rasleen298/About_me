0e508a638e46484c0a8dc95b937ae27a80c9aee8
rasleen298 <rasleen0209@gmail.com>
2019-11-27 16:31:18 +0530

    First version of application moved into github with changes in settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,11 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dajwc*0d5@s!d)=gr40m@)77o9)%e*iy%g+ob+1f0@+1cr(c#6'
+#SECRET_KEY = 'dajwc*0d5@s!d)=gr40m@)77o9)%e*iy%g+ob+1f0@+1cr(c#6'
 
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'dajwc*0d5@s!d)=gr40m@)77o9)%e*iy%g+ob+1f0@+1cr(c#6')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+#DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 ALLOWED_HOSTS = []
 
 
