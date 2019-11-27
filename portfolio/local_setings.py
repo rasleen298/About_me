ffb86577d26af45d23fe9ff2fda7436035e64af1
rasleen298 <rasleen0209@gmail.com>
2019-11-27 15:40:52 +0530

    First version of application moved into github

---
+++ portfolio/local_setings.py
@@ -0,0 +1,14 @@
+SECRET_KEY = 'dajwc*0d5@s!d)=gr40m@3)772o9)%e*2iy%g+ob+1f#0@+1cr(c#6'
+
+ALLOWED_HOSTS = ['159.89.173.184']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'djangohost',
+        'PASSWORD':'078383372ras',
+        'HOST': 'localhost',
+        'PORT':'5432',
+    }
+}
