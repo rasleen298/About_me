90ac0eb54043906a5c979550b544d085cec27117
rasleen298 <rasleen0209@gmail.com>
2019-11-27 21:50:06 +0530

    final changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -149,8 +149,3 @@ DATABASES['default'].update(db_from_env)
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
